09b5b90ed85103d6cbaafe60e75317b00fe8c4f2
aplaxco1 <aplaxco@ucsc.edu>
2023-11-03 12:16:07 -0700

    added timer functionality

--- docs/TheCrane/main.js
+++ docs/TheCrane/main.js
@@ -5,10 +5,27 @@ description = `
 
 characters = [];
 
-options = {};
+options = {
+};
+
+// global variables/consts
+const G = {
+	TIMER_START: 15
+}
 
 function update() {
-	if (!ticks) {
-		
+	if (!ticks) {	
+	}
+	else {
+		// display timer text
+		text("Time: "+time, 3, 10);
 	}
 }
+
+// timer functionailty
+let time = G.TIMER_START;
+let timer = setInterval(() => {
+	if (time > 0) {
+		time--;
+	}
+}, 1000);
